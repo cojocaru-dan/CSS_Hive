cde26dd711fb7c0c5325aaad6e0ddc145b604c7a
cojocaru-dan <dan.cojocaru90233@gmail.com>
2023-04-12 18:07:05 -0700

    Practice CSS basics UI/UX design

--- client/public/script.js
+++ client/public/script.js
@@ -1,7 +1,17 @@
 const loadEvent = function() {
 
   // Write your JavaScript code after this line
+  const root = document.querySelector("#root");
 
+  const element = function (tag, inner) {
+    return `<${tag}>${inner}</${tag}>`;
+  }
+
+  const htmlTags = ["h1", "h2", "h3", "button", "div", "p", "mark", "textarea", "li", "section"];
+  const htmlContents = ["Animation Title", "Hello website", "Let's animate this page", "Click", "Div html element", "Lorem ipsum dolor sit amet", "Mark html element", "Textarea html element", "Coffee", "Section hml element"];
+  for (let i = 0; i < htmlTags.length; i++) {
+    root.insertAdjacentHTML("beforeend", element(htmlTags[i], htmlContents[i]));
+  }
   
   // Write your JavaScript code before this line
 
